42b0b600c25f6e7eb12e613b7b7b109cabc09745
Mykhailo Shemohonov <msh842911@gmail.com>
2020-10-26 16:54:16 +0200

    Home work commit 1

---
+++ src/test/java/tests/Task2Test.java
@@ -0,0 +1,15 @@
+package test.java.tests;
+import main.java.lesson1.Task2;
+
+
+
+public class Task2Test {
+    public static void main(String[] args) {
+        getNamesTest();
+    }
+
+    public static void getNamesTest() {
+        String actualResult = Task2.getNames();
+    }
+}
+
