e9ca681b341423b3b1b304e399db5320bb3d3c55
Mykhailo Shemohonov <msh842911@gmail.com>
2020-11-29 18:51:31 +0200

    Home work new comit

---
+++ src/test/java/tests/TestJenkins.java
@@ -0,0 +1,10 @@
+package test.java.tests;
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("Unit Tests");
+    }
+}
